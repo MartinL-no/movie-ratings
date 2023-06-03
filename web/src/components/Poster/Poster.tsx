5332b81e3516a560e79a93d29bd6adb83f2026c6
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-03 17:46:36 +0200

    feat: create Poster component

---
+++ web/src/components/Poster/Poster.tsx
@@ -0,0 +1,10 @@
+const Poster = () => {
+  return (
+    <div>
+      <h2>{'Poster'}</h2>
+      <p>{'Find me in ./web/src/components/Poster/Poster.tsx'}</p>
+    </div>
+  )
+}
+
+export default Poster
