a3eccf4ba691939d7f98b71534ddd8b4b0642a3c
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-03 17:11:42 +0200

    style: add Abril Fatface and Inter fonts to tailwind theme

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      modern: ['Abril Fatface', 'cursive'],
+      sansSerif: ['Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 }
-
