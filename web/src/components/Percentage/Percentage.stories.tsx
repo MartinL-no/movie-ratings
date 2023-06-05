63c83f838ae2126281d295f7b4223cbec077c4e8
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-05 15:23:03 +0200

    feat(Percentage):  add ring and percentage text

--- web/src/components/Percentage/Percentage.stories.tsx
+++ web/src/components/Percentage/Percentage.stories.tsx
@@ -1,25 +1,23 @@
-// When you've added props to your component,
-// pass Storybook's `args` through this story to control it from the addons panel:
-//
-// ```tsx
-// import type { ComponentStory } from '@storybook/react'
-//
-// export const generated: ComponentStory<typeof Percentage> = (args) => {
-//   return <Percentage {...args} />
-// }
-// ```
-//
 // See https://storybook.js.org/docs/react/writing-stories/args.
 
 import type { ComponentMeta } from '@storybook/react'
 
-import Percentage from './Percentage'
-
-export const generated = () => {
-  return <Percentage />
-}
+import { Percentage, type PercentageProps } from './Percentage'
 
 export default {
   title: 'Components/Percentage',
   component: Percentage,
 } as ComponentMeta<typeof Percentage>
+
+const Template = (args: PercentageProps) => <Percentage {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  percentage: 88,
+}
+
+export const NoRing = Template.bind({})
+NoRing.args = {
+  percentage: 88,
+  ring: false,
+}
