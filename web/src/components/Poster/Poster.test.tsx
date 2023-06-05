d23c0de111941ef3209a6712e48f0e0f180e7a1c
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-05 13:22:12 +0200

    test(Poster): add tests for size and headings

--- web/src/components/Poster/Poster.test.tsx
+++ web/src/components/Poster/Poster.test.tsx
@@ -6,26 +6,59 @@ import Poster from './Poster'
 //    https://redwoodjs.com/docs/testing#testing-components
 
 describe('Poster', () => {
+  const source = './images/spiderman.jpeg'
+  const alt = 'Spiderman'
+
   it('renders successfully', () => {
-    const source = './images/spiderman.jpeg'
-    const alt = 'Spiderman'
     expect(() => {
       render(<Poster alt={alt} src={source} />)
     }).not.toThrow()
   })
 
   it('show the image', () => {
-    const source = './images/spiderman.jpeg'
-    const alt = 'Spiderman'
     render(<Poster alt={alt} src={source} />)
 
     expect(screen.getByRole('img')).toHaveAttribute('src', source)
   })
+
   it('loads the alt text', () => {
-    const source = './images/spiderman.jpeg'
-    const alt = 'Spiderman'
     render(<Poster alt={alt} src={source} />)
 
     expect(screen.getByRole('img')).toHaveAttribute('alt', alt)
+  })
+
+  it('renders a small size', () => {
+    render(<Poster alt={alt} src={source} size="small" />)
+    const poster = screen.getByRole('img')
+    expect(poster).toHaveClass('h-60', { exact: false })
+    expect(poster).toHaveClass('w-40', { exact: false })
+  })
+
+  it('renders a medium size', () => {
+    render(<Poster alt={alt} src={source} size="medium" />)
+    const poster = screen.getByRole('img')
+    expect(poster).toHaveClass('h-[310px]', { exact: false })
+    expect(poster).toHaveClass('w-[224px]', { exact: false })
+  })
+
+  it('renders a large size', () => {
+    render(<Poster alt={alt} src={source} size="large" />)
+    const poster = screen.getByRole('img')
+    expect(poster).toHaveClass('h-[460px]', { exact: false })
+    expect(poster).toHaveClass('w-[300px]', { exact: false })
+  })
+
+  it('renders a heading', () => {
+    const heading = 'I am a heading'
+    render(<Poster alt={alt} src={source} size="large" heading={heading} />)
+    expect(screen.getByText(heading)).toBeInTheDocument()
+  })
+
+  it('renders a subheading', () => {
+    const subheading = 'I am a subheading'
+    render(
+      <Poster alt={alt} src={source} size="large" subheading={subheading} />
+    )
+    expect(screen.getByText(subheading)).toBeInTheDocument()
   })
 })
