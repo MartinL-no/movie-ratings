92f39103f330e44423c5161b3464ede46795976e
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-05 16:13:06 +0200

    docs: add stories for sizes and ratings for Percentage component

--- web/src/components/Percentage/Percentage.stories.tsx
+++ web/src/components/Percentage/Percentage.stories.tsx
@@ -21,3 +21,45 @@ NoRing.args = {
   percentage: 88,
   ring: false,
 }
+
+export const Small = Template.bind({})
+Small.args = {
+  percentage: 88,
+  size: 'small',
+}
+
+export const Medium = Template.bind({})
+Medium.args = {
+  percentage: 88,
+  size: 'medium',
+}
+
+export const Large = Template.bind({})
+Large.args = {
+  percentage: 88,
+  size: 'large',
+}
+
+export const XLarge = Template.bind({})
+XLarge.args = {
+  percentage: 88,
+  size: 'xlarge',
+}
+
+export const LowRating = Template.bind({})
+LowRating.args = {
+  percentage: 30,
+  size: 'medium',
+}
+
+export const MidRating = Template.bind({})
+MidRating.args = {
+  percentage: 60,
+  size: 'medium',
+}
+
+export const HighRating = Template.bind({})
+HighRating.args = {
+  percentage: 90,
+  size: 'medium',
+}
