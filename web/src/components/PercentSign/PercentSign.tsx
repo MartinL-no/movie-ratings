a7fc2cb288d115afb6528f10d8df284b6ffe0f64
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-05 14:43:31 +0200

    feat(PercentSign): create component

---
+++ web/src/components/PercentSign/PercentSign.tsx
@@ -0,0 +1,14 @@
+interface PercentSignProps {
+  // Props
+}
+
+const PercentSign = ({}: PercentSignProps) => {
+  return (
+    <div>
+      <h2>{'PercentSign'}</h2>
+      <p>{'Find me in ./web/src/components/PercentSign/PercentSign.tsx'}</p>
+    </div>
+  )
+}
+
+export { PercentSign, PercentSignProps }
