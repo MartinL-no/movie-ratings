3895dfcf34fb482d30fb261d3aeeebde362d5975
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-06 16:12:23 +0200

    docs(Icons): add story for each icon type

--- web/src/components/Icon/Icon.stories.tsx
+++ web/src/components/Icon/Icon.stories.tsx
@@ -11,7 +11,42 @@ export default {
 
 const Template = (args: IconProps) => <Icon {...args} />
 
-export const Primary = Template.bind({})
-Primary.args = {
-  // args
+export const Arrow = Template.bind({})
+Arrow.args = {
+  name: 'arrow',
+}
+
+export const Bookmark = Template.bind({})
+Bookmark.args = {
+  name: 'bookmark',
+}
+
+export const Close = Template.bind({})
+Close.args = {
+  name: 'close',
+}
+
+export const Hide = Template.bind({})
+Hide.args = {
+  name: 'hide',
+}
+
+export const Search = Template.bind({})
+Search.args = {
+  name: 'search',
+}
+
+export const Show = Template.bind({})
+Show.args = {
+  name: 'show',
+}
+
+export const Star = Template.bind({})
+Star.args = {
+  name: 'star',
+}
+
+export const WatchLater = Template.bind({})
+WatchLater.args = {
+  name: 'watchlater',
 }
