b412c3dc3fa6a2b3ab73e626a36f4e0fe7d915b9
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-06 15:15:58 +0200

    test: add tests for Slider component

--- web/src/components/Slider/Slider.test.tsx
+++ web/src/components/Slider/Slider.test.tsx
@@ -1,14 +1,81 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen } from '@redwoodjs/testing/web'
 
 import { Slider } from './Slider'
+import { slides } from './Slider.mocks'
 
 //   Improve this test with help from the Redwood Testing Doc:
 //    https://redwoodjs.com/docs/testing#testing-components
 
 describe('Slider', () => {
+  const label = 'Most Popular'
+  const slideSize = 'small'
+  const viewAllLink = '/movies'
+
   it('renders successfully', () => {
     expect(() => {
-      render(<Slider />)
+      render(
+        <Slider
+          label={label}
+          slides={slides}
+          slideSize={slideSize}
+          viewAllLink={viewAllLink}
+        />
+      )
     }).not.toThrow()
+  })
+
+  it('displays the section title', () => {
+    render(
+      <Slider
+        label={label}
+        slides={slides}
+        slideSize={slideSize}
+        viewAllLink={viewAllLink}
+      />
+    )
+    expect(screen.getByText(label)).toBeInTheDocument()
+  })
+
+  it('does not display the view all link', () => {
+    render(<Slider label={label} slides={slides} slideSize={slideSize} />)
+    expect(screen.queryByText('View All')).not.toBeInTheDocument()
+  })
+
+  it('displays the view all link', () => {
+    render(
+      <Slider
+        label={label}
+        slides={slides}
+        slideSize={slideSize}
+        viewAllLink={viewAllLink}
+      />
+    )
+    expect(screen.getByText('View All')).toBeInTheDocument()
+  })
+
+  it('links out to the correct place', () => {
+    render(
+      <Slider
+        label={label}
+        slides={slides}
+        slideSize={slideSize}
+        viewAllLink={viewAllLink}
+      />
+    )
+    expect(screen.getByText('View All')).toHaveAttribute('href', viewAllLink)
+  })
+
+  it('displays all the slides', () => {
+    render(
+      <Slider
+        label={label}
+        slides={slides}
+        slideSize={slideSize}
+        viewAllLink={viewAllLink}
+      />
+    )
+    slides.forEach((slide) => {
+      expect(screen.getByAltText(slide.alt)).toBeInTheDocument()
+    })
   })
 })
