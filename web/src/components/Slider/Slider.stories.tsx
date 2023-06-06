904840912d99488c44fee908880cdb5649aed303
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-06 13:49:33 +0200

    docs: add primary story for Slider component

--- web/src/components/Slider/Slider.stories.tsx
+++ web/src/components/Slider/Slider.stories.tsx
@@ -2,6 +2,8 @@
 
 import type { ComponentMeta } from '@storybook/react'
 
+import spiderman from '../Poster/images/spiderman.jpeg'
+
 import { Slider, type SliderProps } from './Slider'
 
 export default {
@@ -13,5 +15,26 @@ const Template = (args: SliderProps) => <Slider {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
+  label: 'Most  Popular',
+  slideSize: 'small',
+  viewAllLink: '/movies',
+  slides: [
+    {
+      src: spiderman,
+      alt: 'Hocus Pocus',
+    },
+    {
+      src: spiderman,
+      alt: 'Hocus Pocus',
+    },
+    {
+      src: spiderman,
+      alt: 'Hocus Pocus',
+    },
+    {
+      src: spiderman,
+      alt: 'Hocus Pocus',
+    },
+  ],
   // args
 }
