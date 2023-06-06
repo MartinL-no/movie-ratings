af7039919635f402f19ecdb161f1bae40f3fc13d
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-06 16:11:56 +0200

    Feat(Icon): Add title elements to subcomponents

--- web/src/components/Icon/Arrow.tsx
+++ web/src/components/Icon/Arrow.tsx
@@ -11,6 +11,7 @@ const Arrow = ({ size = 24 }: ArrowProps) => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
+      <title>Arrow Icon</title>
       <path d="M10.0666 7.40068V24H13.9333V7.40068L18.3663 11.8337L21.1 9.10005L12 0L2.8999 9.10005L5.63359 11.8337L10.0666 7.40068Z" />
     </svg>
   )
