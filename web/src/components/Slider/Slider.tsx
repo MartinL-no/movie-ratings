21a5ca59e9a185c6196ca295162aabef10a2cb89
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-07 16:58:59 +0200

    style(Slider): change max-width to tailwind config variable

--- web/src/components/Slider/Slider.tsx
+++ web/src/components/Slider/Slider.tsx
@@ -12,16 +12,16 @@ interface SliderProps {
 const Slider = ({ label, slides, slideSize, viewAllLink }: SliderProps) => {
   return (
     <>
-      <div className="mx-auto flex max-w-[1300px] items-end justify-between pb-2">
+      <div className="mx-auto flex max-w-pageWidth items-end justify-between pb-2">
         <h2>{label}</h2>
         {viewAllLink && <Link to={viewAllLink}>View All</Link>}
       </div>
-      <div className="overflow-x-scroll whitespace-nowrap pl-[calc((100%-1300px)/2)] snap-x snap-mandatory scroll-pl-[calc((100%-1300px)/2)]">
+      <div className="snap-x snap-mandatory scroll-pl-[calc((100%-1300px)/2)] overflow-x-scroll whitespace-nowrap pl-[calc((100%-1300px)/2)]">
         {slides &&
           slides.map((slide, index: number) => (
             <div
               key={index}
-              className="mr-5 inline-block snap-normal snap-start"
+              className="mr-5 inline-block snap-start snap-normal"
             >
               <Poster
                 alt={slide.alt}
