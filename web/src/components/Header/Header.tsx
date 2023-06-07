b3bdd1bebe075f555e4313eb78716607a98a26fc
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-07 16:36:20 +0200

    feat(Header): create component

---
+++ web/src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+interface HeaderProps {
+  // Props
+}
+
+const Header = ({}: HeaderProps) => {
+  return (
+    <div>
+      <h2>{'Header'}</h2>
+      <p>{'Find me in ./web/src/components/Header/Header.tsx'}</p>
+    </div>
+  )
+}
+
+export { Header, HeaderProps }
