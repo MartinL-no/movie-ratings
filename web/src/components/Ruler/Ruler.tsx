b399c06ddd4dabd7323b93e2c840a577c62a777b
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-07 17:29:04 +0200

    feat(Ruler): create component

---
+++ web/src/components/Ruler/Ruler.tsx
@@ -0,0 +1,14 @@
+interface RulerProps {
+  // Props
+}
+
+const Ruler = ({}: RulerProps) => {
+  return (
+    <div>
+      <h2>{'Ruler'}</h2>
+      <p>{'Find me in ./web/src/components/Ruler/Ruler.tsx'}</p>
+    </div>
+  )
+}
+
+export { Ruler, RulerProps }
