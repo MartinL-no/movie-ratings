028b86976801c3ccbaca17edfaf14bc1ef448f53
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-07 16:06:57 +0200

    feat(HomeHeader): create component

---
+++ web/src/components/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,14 @@
+interface HomeHeaderProps {
+  // Props
+}
+
+const HomeHeader = ({}: HomeHeaderProps) => {
+  return (
+    <div>
+      <h2>{'HomeHeader'}</h2>
+      <p>{'Find me in ./web/src/components/HomeHeader/HomeHeader.tsx'}</p>
+    </div>
+  )
+}
+
+export { HomeHeader, HomeHeaderProps }
