b1f01b2680268a8e0b17c5037e63e2e31ec23c88
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-07 12:53:19 +0200

    test(Slide): add tests for title, image and percentage

--- web/src/components/Feature/Slide/Slide.test.tsx
+++ web/src/components/Feature/Slide/Slide.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen } from '@redwoodjs/testing/web'
 
 import { Slide } from './Slide'
 
@@ -6,9 +6,31 @@ import { Slide } from './Slide'
 //    https://redwoodjs.com/docs/testing#testing-components
 
 describe('Slide', () => {
+  const image =
+    'https://www.themoviedb.org/t/p/original/lcTuggU70y6pt6x13Rv1Ffjs93K.jpg'
+  const percentage = 89
+  const title = 'Money Heist: Korea - Joint Economic Area'
+
   it('renders successfully', () => {
     expect(() => {
-      render(<Slide />)
+      render(<Slide image={image} percentage={percentage} title={title} />)
     }).not.toThrow()
+  })
+
+  it('displays the correct title', () => {
+    render(<Slide image={image} percentage={percentage} title={title} />)
+    expect(screen.getByText(title)).toBeInTheDocument()
+  })
+
+  it('displays the correct image', () => {
+    render(<Slide image={image} percentage={percentage} title={title} />)
+    expect(screen.getByTestId('slide')).toHaveStyle(
+      `background-image: linear-gradient(90deg, #000000 6.62%, rgba(0,0,0,0) 71.78%), url(${image})`
+    )
+  })
+
+  it('displays the correct percentage', () => {
+    render(<Slide image={image} percentage={percentage} title={title} />)
+    expect(screen.getByText(percentage)).toBeInTheDocument()
   })
 })
