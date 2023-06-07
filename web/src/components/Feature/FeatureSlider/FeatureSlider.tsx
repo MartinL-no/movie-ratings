ef5f0ed7ff695e8f7d47158e43c617828d84a7c3
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-06-07 12:59:13 +0200

    feat(FeatureSlider): create component

---
+++ web/src/components/Feature/FeatureSlider/FeatureSlider.tsx
@@ -0,0 +1,14 @@
+interface FeatureSliderProps {
+  // Props
+}
+
+const FeatureSlider = ({}: FeatureSliderProps) => {
+  return (
+    <div>
+      <h2>{'FeatureSlider'}</h2>
+      <p>{'Find me in ./web/src/components/FeatureSlider/FeatureSlider.tsx'}</p>
+    </div>
+  )
+}
+
+export { FeatureSlider, FeatureSliderProps }
